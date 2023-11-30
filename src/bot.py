95b4bcd2f18c695caacdf27d88ff302e83f32eb0
resgjk <aremijplohih@gmail.com>
2023-11-30 19:15:54 +0500

    hello handler

--- src/bot.py
+++ src/bot.py
@@ -1,19 +1,28 @@
-import asyncio
-
 from config import BOT_TOKEN
 
-from aiogram import Bot
-from aiogram import Dispatcher
-from aiogram import types
+import asyncio
+import logging
+import sys
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
 
-bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
+@dp.message(CommandStart())
+async def cmd_start_handler(message: Message):
+    await message.answer('Hello!')
+
+
 async def main():
+    bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)
     await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
