02b7249f41759b25f9e370f0f5ae93bdedfa2534
resgjk <aremijplohih@gmail.com>
2024-04-23 19:24:33 +0500

    some fixes

--- CleoverBot/admins_core/keyboards/projects_route_keyboard.py
+++ CleoverBot/admins_core/keyboards/projects_route_keyboard.py
@@ -5,7 +5,7 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 def get_projects_route_keyboard() -> InlineKeyboardMarkup:
     keyboard_builder = InlineKeyboardBuilder()
     keyboard_builder.button(text="➕ Добавить проект", callback_data="add_project")
-    keyboard_builder.button(text="🚫 Удалить проект", callback_data="delete_project")
+    keyboard_builder.button(text="👉 Выбрать проект", callback_data="choise_project")
     keyboard_builder.button(text="⬅️ Назад", callback_data="projects_settings")
     keyboard_builder.adjust(1, repeat=True)
     return keyboard_builder.as_markup()
